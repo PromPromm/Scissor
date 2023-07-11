abf0e42561ca1796356e6e572c62dddbcc5eec6c
Falaye Promise <promiseanuoluwa@gmail.com>
2023-07-11 21:19:55 +0100

    Add jwt error handlers

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restx import Api
 from .utils import db, limiter
 from flask_migrate import Migrate
@@ -96,5 +96,51 @@ def create_app(config=config_dict["dev"]):
         if email == configuration("EMAIL"):
             return {"super_admin": True}
         return {"super_admin": False}
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+        jti = jwt_payload["jti"]
+        token = TokenBlocklist.query.filter_by(jti=jti).first()
+        return token is not None
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
 
     return app
